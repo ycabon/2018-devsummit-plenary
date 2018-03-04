817cda4fd767bc0af0ee10e863583c778c07660e
Yann Cabon <ycabon@esri.com>
2018-03-04 13:04:30 -0800

    fix layer scope

--- src/2-hurricanes/application.ts
+++ src/2-hurricanes/application.ts
@@ -339,7 +339,7 @@ function toggleHighlighting() {
 
 // For mobile support
 function addCSVLayer() {
-  const layer = new CSVLayer({
+  layer = new CSVLayer({
     url: "src/2-hurricanes/Hurricanes.csv",
     // "PROJCS[\"South Pole Stereographic_1\",GEOGCS[\"GCS WGS 1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.0,298.257223563]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Stereographic\"],PARAMETER[\"False_Easting\",0.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",-145.0],PARAMETER[\"Scale_Factor\",1.0],PARAMETER[\"Latitude_Of_Origin\",-90.0],UNIT[\"Meter\",1.0]]"
     spatialReference: view.spatialReference,
